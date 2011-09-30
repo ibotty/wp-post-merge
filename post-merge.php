55cefa92017a772287367d8fc3b2ba88bbaa621c
Tobias Florek <me@ibotty.net>
2011-09-30 18:50:20 +0200

    initial commit

---
+++ post-merge.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Post-Merge
+ * Plugin URI: http://github.com/ibotty/post-merge
+ * Description: a plugin to merge two posts
+ * Version: 0.0
+ * Author: Tobias Florek <me@ibotty.net>
+ * Author URI: http://github.com/ibotty
+ * License: 2-clause BSD
+ */
+?>
